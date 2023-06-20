28f3472c4b2aadcd22b22eb203b37783a891c7a5
RabihMoustapha <mostapharabih59@gmail.com>
2023-06-20 09:15:22 +0300

    Shortcut pages

--- PHP/Log-out.php
+++ PHP/Log-out.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Log-out</title>
-</head>
-
-<body>
     <?php
     session_start();
     session_destroy();
     header("Location:Login-Page.php");
     ?>
-</body>
-
-</html>
