965f3f9c27a256705a633dde7dfa18cacdaf6757
RabihMoustapha <mostapharabih59@gmail.com>
2023-06-25 11:59:26 +0300

    Add admin login acees

--- PHP/admin/LoginAdmin.php
+++ PHP/admin/LoginAdmin.php
@@ -34,7 +34,7 @@
         <h2>
             LOGIN
         </h2>
-        <form action="LogAdmin.php" method="post">
+        <form action="LoginAdminAccess.php" method="post">
             <div class="input-box">
                 <span class="icon">
                     <ion-icon name="mail-outline"></ion-icon>
